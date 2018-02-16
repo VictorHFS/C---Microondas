69d9f8c6b23bfae5544bc7aa5834fd25319fa732
VictorHFS <vhfurtuoso@gmail.com>
2018-02-16 19:26:15 -0200

    inicializado teste unitario

---
+++ MicroondasUnitTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApplication1.Microondas;
+namespace MicroondasUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        MicroondasController controller;
+
+        [TestMethod]
+        public void TestarAquecimentoRapidoSemEntrada()
+        {
+            controller = new MicroondasController(new ProgramasCadastrados(), "");
+            controller.aquecimentoRapido();            
+        }
+
+        [TestMethod]
+        public void TestarAquecimentoRapidoComEntrada()
+        {
+            controller = new MicroondasController(new ProgramasCadastrados(), "Peixe");
+            controller.aquecimentoRapido();
+
+            controller = new MicroondasController(new ProgramasCadastrados(), "Nao Existe");
+            controller.aquecimentoRapido();            
+        }
+    }
+}
